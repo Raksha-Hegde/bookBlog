4154ce64365c5ac4f9649974db4c14e34a5a41d2
hegderaksha5@gmail.com <raksha.rhegde@wipro.com>
2019-02-23 14:54:01 +0530

    Basic Template

--- WebContent/function.js
+++ WebContent/function.js
@@ -1,6 +1,10 @@
 
 function sendMail() {
-	"mailto: "
-	name = document.getElementById(name).g;
-	prompt("MMMM", name);
+	var link = 'mailto:hegderaksha5@gmail.com?subject=Message from '
+	+ document.getElementById('names').value
+	+ '&body=' + document.getElementById('message').value;
+//	window.location.href = link;
+//	prompt("rtyu", link)
+//	document.getElementById('message').value = link;
+	return link;
 }
